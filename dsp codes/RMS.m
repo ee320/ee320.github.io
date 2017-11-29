55c5fee6af86a3d9c5bf90a2559d594d1937194d
ee320 <33447317+ee320@users.noreply.github.com>
2017-11-29 16:19:22 +0530

    Add files via upload

---
+++ dsp codes/RMS.m
@@ -0,0 +1,6 @@
+function [x] = RMS(y)
+x = 0;
+for i=1:length(y),
+   x = x+y(i)*y(i);
+end
+x =sqrt(x); 
---
+++ dsp codes/Zero_Crossings.m
@@ -0,0 +1,7 @@
+function [zc] = Zero_Crossings(y)
+zc = 0;
+for i=2:length(y),
+   if y(i)*y(i-1) < 0,
+      zc = zc+1;
+   end,
+end,
